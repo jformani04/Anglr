699e77c167a9710275e235fd35f46f3985d49c7d
Jason Formani <jformani04@outlook.com>
2026-01-27 17:32:10 -0500

    basic app/ layout

---
+++ app/(auth)/forgot_password.tsx
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function forgot_password() {
+  return (
+    <View>
+      <Text>forgot-password</Text>
+    </View>
+  )
+}
+
+
+const styles = StyleSheet.create({})
---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,9 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function TabsLayout() {
+ 
+}
+
+
+const styles = StyleSheet.create({})
